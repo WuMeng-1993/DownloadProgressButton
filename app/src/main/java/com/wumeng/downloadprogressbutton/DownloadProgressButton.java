55df4cd4b8aa304e449558dc61a6697c63cc2503
Wu Meng <meng.wu@ecarx.com.cn>
2020-05-04 20:40:09 +0800

    添加代码

--- app/src/main/java/com/wumeng/downloadprogressbutton/DownloadProgressButton.java
+++ app/src/main/java/com/wumeng/downloadprogressbutton/DownloadProgressButton.java
@@ -1,9 +1,15 @@
 package com.wumeng.downloadprogressbutton;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.view.View;
 
 /**
  * @author WuMeng
@@ -37,6 +43,53 @@ public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatT
      */
     private int mTextCoverColor;
 
+    /**
+     * 边框高度
+     */
+    private float background_strokeWidth;
+
+    /**
+     *
+     */
+    private String mNormalText,mDowningText,mFinishText,mPauseText;
+
+
+    private int mMaxProgress,mMinProgress;
+
+    private float mProgress = -1,mToProgress;
+
+    /**
+     * 背景画笔, 背景边框画笔
+     */
+    private Paint mBackgroundPaint,mBackgroundBorderPaint;
+
+    /**
+     * 按钮文字画笔
+     */
+    private volatile Paint mTextPaint;
+
+    private int mState = -1;
+
+    /**
+     * 4中状态
+     */
+    public static final int NORMAL = 1,DOWNLOADING = 2,PAUSE = 3,FINISH = 4;
+
+    /**
+     * 当前显示的文本
+     */
+    private CharSequence mCurrentText;
+
+    /**
+     * 属性动画
+     */
+    private ValueAnimator mProgressAnimation;
+
+    /**
+     * 动画持续的时间
+     */
+    private long mAnimationDuration;
+
     public DownloadProgressButton(Context context) {
         this(context,null);
     }
@@ -46,6 +99,8 @@ public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatT
         // isInEditMode: 视图处于编辑模式
         if (!isInEditMode()) {
             initAttrs(context,attrs);
+            init();
+            setupAnimations();
         }
     }
 
@@ -56,13 +111,159 @@ public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatT
         mButtonRadius = typedArray.getFloat(R.styleable.DownloadProgressButton_radius,getMeasuredHeight() / 2);
         mTextColor = typedArray.getColor(R.styleable.DownloadProgressButton_text_color,mBackgroundColor);
         mTextCoverColor = typedArray.getColor(R.styleable.DownloadProgressButton_text_covercolor,Color.WHITE);
+        background_strokeWidth = typedArray.getDimension(R.styleable.DownloadProgressButton_background_strokeWidth,3F);
+        mNormalText = typedArray.getString(R.styleable.DownloadProgressButton_text_normal);
+        mDowningText = typedArray.getString(R.styleable.DownloadProgressButton_text_downing);
+        mPauseText = typedArray.getString(R.styleable.DownloadProgressButton_text_pause);
+        mFinishText = typedArray.getString(R.styleable.DownloadProgressButton_text_finish);
+        mAnimationDuration = typedArray.getInt(R.styleable.DownloadProgressButton_animation_duration,500);
 
         typedArray.recycle();
     }
 
+    private void init() {
+        mMaxProgress = 100;
+        mMinProgress = 0;
+        mProgress = 0;
+        if (mNormalText == null) {
+            mNormalText = "下载";
+        }
 
-    public DownloadProgressButton(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        if (mDowningText == null) {
+            mNormalText = "";
+        }
+
+        if (mPauseText == null) {
+            mPauseText = "继续";
+        }
+
+        if (mFinishText == null) {
+            mFinishText = "使用";
+        }
+
+        // 背景画笔
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setAntiAlias(true);
+        mBackgroundPaint.setStyle(Paint.Style.FILL);
+
+        // 背景边框画笔
+        mBackgroundBorderPaint = new Paint();
+        mBackgroundBorderPaint.setAntiAlias(true);
+        mBackgroundBorderPaint.setStyle(Paint.Style.STROKE);
+        mBackgroundBorderPaint.setStrokeWidth(background_strokeWidth);
+        mBackgroundBorderPaint.setColor(mBackgroundColor);
+
+        // 按钮文字画笔
+        mTextPaint = new Paint();
+        mTextPaint.setAntiAlias(true);
+        setLayerType(LAYER_TYPE_SOFTWARE,mTextPaint);
+
+        // 初始化状态为Normal
+        setState(NORMAL);
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    /**
+     *
+     */
+    private void setupAnimations() {
+        mProgressAnimation = ValueAnimator.ofFloat(0,1).setDuration(mAnimationDuration);
+        mProgressAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float timePercent = (float) animation.getAnimatedValue();
+                mProgress = ((mToProgress - mProgress) * timePercent + mProgress);
+                setProgressText(mProgress);
+            }
+        });
+        mProgressAnimation.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                super.onAnimationCancel(animation);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (mProgress == mMaxProgress) {
+                    setState(FINISH);
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                super.onAnimationRepeat(animation);
+            }
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+                if (mToProgress < mProgress) {
+                    mProgress = mToProgress;
+                }
+            }
+        });
+    }
+
+    /**
+     * 设置控件状态
+     * @param state
+     */
+    private void setState(int state) {
+        // 状态确实有改变
+        if (mState != state) {
+            switch (state) {
+                case NORMAL:
+                    mProgress = mToProgress = mMinProgress;
+                    setCurrentText(mNormalText);
+                    break;
+                case PAUSE:
+                    setCurrentText(mPauseText);
+                    break;
+                case FINISH:
+                    setCurrentText(mFinishText);
+                    mProgress = mMaxProgress;
+                    break;
+                default:
+                    break;
+            }
+            invalidate();
+        }
+    }
+
+    /**
+     * 获取当前的状态
+     * @return
+     */
+    public int getState() {
+        return mState;
+    }
+
+    /**
+     * 设置当前的文本
+     * @param charSequence
+     */
+    private void setCurrentText(CharSequence charSequence) {
+        mCurrentText = charSequence;
+        invalidate();
+    }
+
+    private void setProgressText(float progress) {
+        if (progress <= mMinProgress || progress <= mToProgress || getState() == FINISH) {
+            return;
+        }
+        mToProgress = Math.min(progress,mMaxProgress);
+        setState(DOWNLOADING);
+        if (mProgressAnimation.isRunning()) {
+            mProgressAnimation.end();
+            mProgressAnimation.start();
+        } else {
+            mProgressAnimation.start();
+        }
     }
 
 }
