289e212cb760921bcb5905ec7150b4e0c8c2a2f9
Wu Meng <meng.wu@ecarx.com.cn>
2020-05-06 20:24:33 +0800

    添加代码

--- app/src/main/java/com/wumeng/downloadprogressbutton/DownloadProgressButton.java
+++ app/src/main/java/com/wumeng/downloadprogressbutton/DownloadProgressButton.java
@@ -5,9 +5,12 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
-import android.os.Build;
+import android.graphics.RectF;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -17,6 +20,41 @@ import android.view.View;
  * desc:
  */
 public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatTextView {
+
+    public interface OnDownLoadClickListener {
+
+        void clickDownload();
+
+        void clickPause();
+
+        void clickResume();
+
+        void clickFinish();
+
+    }
+
+    public static class SimpleOnDownLoadClickListener implements OnDownLoadClickListener {
+
+        @Override
+        public void clickDownload() {
+
+        }
+
+        @Override
+        public void clickPause() {
+
+        }
+
+        @Override
+        public void clickResume() {
+
+        }
+
+        @Override
+        public void clickFinish() {
+
+        }
+    }
 
     /**
      * 背景颜色
@@ -29,19 +67,29 @@ public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatT
     private int mBackgroundSecondColor;
 
     /**
-     * 按钮的角度
+     * 背景画笔, 背景边框画笔
+     */
+    private Paint mBackgroundPaint, mBackgroundBorderPaint;
+
+    /**
+     * 按钮的圆角角度
      */
     private float mButtonRadius;
 
     /**
-     * 文字颜色
+     * 按钮文字画笔
      */
-    private int mTextColor;
+    private volatile Paint mTextPaint;
 
     /**
-     * 覆盖后颜色
+     * 文字颜色,覆盖后文字颜色
      */
-    private int mTextCoverColor;
+    private int mTextColor, mTextCoverColor;
+
+    /**
+     * 不同状态下显示的文字
+     */
+    private String mNormalText, mDowningText, mFinishText, mPauseText;
 
     /**
      * 边框高度
@@ -49,31 +97,21 @@ public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatT
     private float background_strokeWidth;
 
     /**
-     *
+     * progress的最大值，最小值
      */
-    private String mNormalText,mDowningText,mFinishText,mPauseText;
+    private int mMaxProgress, mMinProgress;
 
-
-    private int mMaxProgress,mMinProgress;
-
-    private float mProgress = -1,mToProgress;
+    private float mProgress = -1, mToProgress;
 
     /**
-     * 背景画笔, 背景边框画笔
+     * 当前的状态
      */
-    private Paint mBackgroundPaint,mBackgroundBorderPaint;
-
-    /**
-     * 按钮文字画笔
-     */
-    private volatile Paint mTextPaint;
-
     private int mState = -1;
 
     /**
-     * 4中状态
+     * 按钮的4中状态
      */
-    public static final int NORMAL = 1,DOWNLOADING = 2,PAUSE = 3,FINISH = 4;
+    public static final int NORMAL = 1, DOWNLOADING = 2, PAUSE = 3, FINISH = 4;
 
     /**
      * 当前显示的文本
@@ -90,35 +128,59 @@ public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatT
      */
     private long mAnimationDuration;
 
+    private RectF mBackgroundBounds;
+
+    private LinearGradient mProgressBgGradient,mProgressTextGradient;
+
+    /**
+     * 当前下载的百分比
+     */
+    private float mProgressPercent;
+
+    private OnDownLoadClickListener mOnDownLoadClickListener;
+
+
     public DownloadProgressButton(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public DownloadProgressButton(Context context, AttributeSet attrs) {
         super(context, attrs);
         // isInEditMode: 视图处于编辑模式
         if (!isInEditMode()) {
-            initAttrs(context,attrs);
+            initAttrs(context, attrs);
             init();
             setupAnimations();
         }
     }
 
+    /**
+     * 获取自定义属性
+     * @param context
+     * @param attrs
+     */
     private void initAttrs(Context context, AttributeSet attrs) {
-        TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.DownloadProgressButton);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.DownloadProgressButton);
         mBackgroundColor = typedArray.getColor(R.styleable.DownloadProgressButton_backgroud_color, Color.parseColor("#6699ff"));
-        mBackgroundSecondColor = typedArray.getColor(R.styleable.DownloadProgressButton_background_second_color,Color.LTGRAY);
-        mButtonRadius = typedArray.getFloat(R.styleable.DownloadProgressButton_radius,getMeasuredHeight() / 2);
-        mTextColor = typedArray.getColor(R.styleable.DownloadProgressButton_text_color,mBackgroundColor);
-        mTextCoverColor = typedArray.getColor(R.styleable.DownloadProgressButton_text_covercolor,Color.WHITE);
-        background_strokeWidth = typedArray.getDimension(R.styleable.DownloadProgressButton_background_strokeWidth,3F);
+        mBackgroundSecondColor = typedArray.getColor(R.styleable.DownloadProgressButton_background_second_color, Color.LTGRAY);
+        mButtonRadius = typedArray.getFloat(R.styleable.DownloadProgressButton_radius, getMeasuredHeight() / 2);
+        mTextColor = typedArray.getColor(R.styleable.DownloadProgressButton_text_color, mBackgroundColor);
+        mTextCoverColor = typedArray.getColor(R.styleable.DownloadProgressButton_text_covercolor, Color.WHITE);
+        background_strokeWidth = typedArray.getDimension(R.styleable.DownloadProgressButton_background_strokeWidth, 3F);
         mNormalText = typedArray.getString(R.styleable.DownloadProgressButton_text_normal);
         mDowningText = typedArray.getString(R.styleable.DownloadProgressButton_text_downing);
         mPauseText = typedArray.getString(R.styleable.DownloadProgressButton_text_pause);
         mFinishText = typedArray.getString(R.styleable.DownloadProgressButton_text_finish);
-        mAnimationDuration = typedArray.getInt(R.styleable.DownloadProgressButton_animation_duration,500);
+        mAnimationDuration = typedArray.getInt(R.styleable.DownloadProgressButton_animation_duration, 500);
 
         typedArray.recycle();
+    }
+
+    @Override
+    public void setTextSize(float size) {
+        super.setTextSize(size);
+        mTextPaint.setTextSize(size);
+        invalidate();
     }
 
     private void init() {
@@ -130,7 +192,7 @@ public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatT
         }
 
         if (mDowningText == null) {
-            mNormalText = "";
+            mDowningText = "";
         }
 
         if (mPauseText == null) {
@@ -156,13 +218,16 @@ public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatT
         // 按钮文字画笔
         mTextPaint = new Paint();
         mTextPaint.setAntiAlias(true);
-        setLayerType(LAYER_TYPE_SOFTWARE,mTextPaint);
+        setLayerType(LAYER_TYPE_SOFTWARE, mTextPaint);
 
         // 初始化状态为Normal
         setState(NORMAL);
         setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (mOnDownLoadClickListener == null) {
+                    return;
+                }
 
             }
         });
@@ -173,7 +238,7 @@ public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatT
      *
      */
     private void setupAnimations() {
-        mProgressAnimation = ValueAnimator.ofFloat(0,1).setDuration(mAnimationDuration);
+        mProgressAnimation = ValueAnimator.ofFloat(0, 1).setDuration(mAnimationDuration);
         mProgressAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
@@ -211,11 +276,13 @@ public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatT
 
     /**
      * 设置控件状态
+     *
      * @param state
      */
     private void setState(int state) {
         // 状态确实有改变
         if (mState != state) {
+            this.mState = state;
             switch (state) {
                 case NORMAL:
                     mProgress = mToProgress = mMinProgress;
@@ -237,6 +304,7 @@ public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatT
 
     /**
      * 获取当前的状态
+     *
      * @return
      */
     public int getState() {
@@ -245,6 +313,7 @@ public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatT
 
     /**
      * 设置当前的文本
+     *
      * @param charSequence
      */
     private void setCurrentText(CharSequence charSequence) {
@@ -256,7 +325,7 @@ public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatT
         if (progress <= mMinProgress || progress <= mToProgress || getState() == FINISH) {
             return;
         }
-        mToProgress = Math.min(progress,mMaxProgress);
+        mToProgress = Math.min(progress, mMaxProgress);
         setState(DOWNLOADING);
         if (mProgressAnimation.isRunning()) {
             mProgressAnimation.end();
@@ -265,5 +334,138 @@ public class DownloadProgressButton extends androidx.appcompat.widget.AppCompatT
             mProgressAnimation.start();
         }
     }
+
+    /**
+     * 绘制
+     * @param canvas
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (!isInEditMode()) {
+            drawing(canvas);
+        }
+    }
+
+    /**
+     * 绘制背景和文字
+     * @param canvas
+     */
+    private void drawing(Canvas canvas) {
+        drawBackground(canvas);
+        drawTextAbove(canvas);
+    }
+
+    /**
+     * 绘制背景
+     * @param canvas
+     */
+    private void drawBackground(Canvas canvas) {
+        if (mBackgroundBounds == null) {
+            mBackgroundBounds = new RectF();
+            if (mButtonRadius == 0) {
+                mButtonRadius = getMeasuredHeight() / 2;
+            }
+            mBackgroundBounds.left = background_strokeWidth;
+            mBackgroundBounds.top = background_strokeWidth;
+            mBackgroundBounds.right = getMeasuredWidth() - background_strokeWidth;
+            mBackgroundBounds.bottom = getMeasuredHeight() - background_strokeWidth;
+        }
+
+        switch (mState) {
+            case NORMAL:
+                break;
+            case DOWNLOADING:
+            case PAUSE:
+                mProgressPercent = mProgress / (mMaxProgress + 0f);
+                mProgressBgGradient = new LinearGradient(background_strokeWidth,0,
+                        getMeasuredWidth() - background_strokeWidth, 0,
+                        new int[]{mBackgroundColor,mBackgroundSecondColor},
+                        new float[]{mProgressPercent,mProgressPercent + 0.001f},
+                        Shader.TileMode.CLAMP
+                );
+                mBackgroundPaint.setColor(mBackgroundColor);
+                mBackgroundPaint.setShader(mProgressBgGradient);
+                canvas.drawRoundRect(mBackgroundBounds,mButtonRadius,mButtonRadius,mBackgroundPaint);
+                break;
+            case FINISH:
+                mBackgroundPaint.setShader(null);
+                mBackgroundPaint.setColor(mBackgroundColor);
+                canvas.drawRoundRect(mBackgroundBounds,mButtonRadius,mButtonRadius,mBackgroundPaint);
+                break;
+            default:
+                break;
+        }
+        // 绘制边框
+        canvas.drawRoundRect(mBackgroundBounds,mButtonRadius,mButtonRadius,mBackgroundBorderPaint);
+    }
+
+    /**
+     * 绘制文本
+     * @param canvas
+     */
+    private void drawTextAbove(Canvas canvas) {
+        mTextPaint.setTextSize(getTextSize());
+        final float y = canvas.getHeight() / 2 - (mTextPaint.descent() / 2 + mTextPaint.ascent() / 2);
+        if (mCurrentText == null) {
+            mCurrentText = "";
+        }
+        final float textWidth = mTextPaint.measureText(mCurrentText.toString());
+        switch (mState) {
+            case NORMAL:
+                mTextPaint.setShader(null);
+                mTextPaint.setColor(mTextColor);
+                canvas.drawText(mCurrentText.toString(),(getMeasuredWidth() - textWidth)/2,y,mTextPaint);
+                break;
+            case DOWNLOADING:
+            case PAUSE:
+                float w = getMeasuredWidth() -2 * background_strokeWidth;
+                float coverLength = w * mProgressPercent;
+                float indicator1 = w / 2 - textWidth / 2;
+                float indicator2 = w / 2 + textWidth / 2;
+                float coverTextLength = textWidth / 2 - w / 2 + coverLength;
+                float textProgress = coverTextLength / textWidth;
+                if (coverLength <= indicator1) {
+                    mTextPaint.setShader(null);
+                    mTextPaint.setColor(mTextColor);
+                } else if (indicator1 < coverLength && coverLength <= indicator2) {
+                    mProgressTextGradient = new LinearGradient((w - textWidth) / 2 + background_strokeWidth,0,
+                            (w + textWidth) / 2 + background_strokeWidth,0,
+                            new int[]{mTextCoverColor,mTextColor},
+                            new float[]{textProgress,textProgress + 0.001f},
+                            Shader.TileMode.CLAMP);
+                    mTextPaint.setColor(mTextColor);
+                    mTextPaint.setShader(mProgressTextGradient);
+                } else {
+                    mTextPaint.setShader(null);
+                    mTextPaint.setColor(mTextCoverColor);
+                }
+                canvas.drawText(mCurrentText.toString(),(w - textWidth) / 2 + background_strokeWidth,y,mTextPaint);
+                break;
+            case FINISH:
+                mTextPaint.setColor(mTextCoverColor);
+                canvas.drawText(mCurrentText.toString(),(getMeasuredWidth() - textWidth)/2,y,mTextPaint);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 设置接口实例
+     * @param onDownLoadClickListener
+     */
+    public void setOnDownLoadClickListener(OnDownLoadClickListener onDownLoadClickListener) {
+        this.mOnDownLoadClickListener = onDownLoadClickListener;
+    }
+
+    /**
+     * 获取接口实例
+     * @return
+     */
+    public OnDownLoadClickListener getmOnDownLoadClickListener() {
+        return mOnDownLoadClickListener;
+    }
+
 
 }
